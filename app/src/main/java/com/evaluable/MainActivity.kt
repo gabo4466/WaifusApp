075fa0fcde31164914184f9532aa162dd4e72e06
Gabriel <costanzag1999@gmail.com>
2023-01-15 14:15:29 +0100

    Login view model

--- app/src/main/java/com/evaluable/MainActivity.kt
+++ app/src/main/java/com/evaluable/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.evaluable
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,7 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.evaluable.navigation.AppNavigation
+import com.evaluable.ui.screens.LoginViewModel
 import com.evaluable.ui.theme.EvaluableTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +34,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            val viewModel: LoginViewModel by viewModels()
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            viewModel.finishLogIn(task)
+        }
+    }
+
 }
 
