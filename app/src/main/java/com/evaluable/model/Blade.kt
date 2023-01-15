9432d06d1d158db7a805f364688e1a2ec3bed871
Gabriel <costanzag1999@gmail.com>
2023-01-15 19:41:48 +0100

    Blades List, data retrieve works

---
+++ app/src/main/java/com/evaluable/model/Blade.kt
@@ -0,0 +1,5 @@
+package com.evaluable.model
+
+class Blade(var name: String, var description: String?, var element: String?) {
+
+}
--- app/src/main/java/com/evaluable/ui/screens/ListBlades.kt
+++ app/src/main/java/com/evaluable/ui/screens/ListBlades.kt
@@ -1,24 +1,57 @@
 package com.evaluable.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.evaluable.R
+import com.evaluable.model.Blade
 import com.evaluable.ui.TopBar
+import com.google.firebase.firestore.FirebaseFirestore
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ListBlades(navController: NavController, email: String?) {
 
     var user by rememberSaveable { mutableStateOf("") }
+    val bladesCollectionName = stringResource(id = R.string.collection_blades)
+    val usersCollectionName = stringResource(id = R.string.collection_users)
+    var blades = rememberSaveable { mutableListOf<Blade>() }
+    val error = stringResource(id = R.string.error_generic)
+    var message by rememberSaveable { mutableStateOf("") }
+    var isLoading by rememberSaveable { mutableStateOf(true) }
+
+    val db = FirebaseFirestore.getInstance()
+
+    if (email != null) {
+        db.collection(usersCollectionName)
+            .document(email)
+            .collection(bladesCollectionName)
+            .get()
+            .addOnSuccessListener {
+                blades.clear()
+                for (blade in it) {
+                    val auxBlade = Blade(blade.get("name") as String, blade.get("description") as String?, blade.get("element") as String?)
+                    blades.add(auxBlade)
+                }
+            }
+            .addOnFailureListener {
+                message = error
+            }
+            .addOnCompleteListener {
+                isLoading = false
+            }
+    }
 
     email?.let {
         user = it
@@ -29,7 +62,37 @@ fun ListBlades(navController: NavController, email: String?) {
         modifier = Modifier.fillMaxSize(),
         topBar = { TopBar(navController = navController, pageName = stringResource(id = R.string.blade_list)
         ) }
-    ) {}
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = message, color = MaterialTheme.colors.primary)
+            if (isLoading) {
+                Spacer(modifier = Modifier.size(30.dp))
+                CircularProgressIndicator()
+            }
 
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 30.dp),
+            ) {
+                for (blade in blades) {
+                    Log.d("BLADE", blade.name)
+                    BladeItem(blade = blade)
+                }
 
+            }
+        }
+    }
+    
+}
+
+@Composable
+fun BladeItem(blade: Blade) {
+    Text(text = blade.name)
 }
